c61dc2238cef5db4710f58c14d5b54d96d332be1
qvinhb1910177 <marcoving1606@gmail.com>
2023-02-13 01:16:20 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route") ;
 
 const app = express();
- 
+const ApiError = require("./app/api-error");
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
-app.get("/", (req, res) => {
+
+/* app.get("/", (req, res) => {
     res.json({ message: "welcom to my contact book application. "});
+}); */
+//handle 404 response
+app.use((req, res, next)=>{
+
+    return next(new ApiError(404, "Resource not found"));
 });
- 
+
+app.use((err, req, res, next)=>{
+    
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+
 module.exports = app;
+
